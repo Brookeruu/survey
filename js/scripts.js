f0e64e2570b175f894b1d20bd878fb353cd6a511
Brooke Perry and Paige Williams <brooke@>
2018-10-17 15:20:17 -0700

    Add submit buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-var favFood = $("#favFood").val();
+$(document).ready(function() {
+  var favFood = $("#favFood").val();
 
-var music = $("input:radio[name=music]:checked").val();
+  var music = $("input:radio[name=music]:checked").val();
 
-var dob = $("#born").val();
+  var dob = $("#born").val();
+
+  var color = $("#favColor").val();
+
+
+
+
+
+
+
+
+
+});
